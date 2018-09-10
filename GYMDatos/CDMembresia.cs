7c1442c527925f248869e2af0256c45c88249584
tuenki <42508161+tuenki@users.noreply.github.com>
2018-09-09 23:39:53 -0700

    mio

---
+++ GYMDatos/CDMembresia.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace GYMDatos
+{
+    public class CDMembresia
+    {
+        private DBConexion conexion = new DBConexion();
+        SqlDataReader read;
+        DataTable table = new DataTable();
+        SqlCommand comand = new SqlCommand();
+
+        private int _Id;
+        private String _NameMembership;
+        private int _Days;
+        private int _IDType;
+        private decimal _Price;
+
+        public int ID
+        {
+            set { _Id = value; }
+            get { return _Id; }
+        }
+        public String NameMembership
+        {
+            set { _NameMembership = value; }
+            get { return _NameMembership; }
+        }
+
+        public int Days
+        {
+            set { _Days = value; }
+            get { return _Days; }
+        }
+
+        public int IdType
+        {
+            set { _IDType = value; }
+            get { return _IDType; }
+        }
+
+        public decimal Price
+        {
+            set { _Price = value; }
+            get { return _Price; }
+        }
+        
+        // mostrar membresias
+        public DataTable ShowListMembership()
+        {
+            comand.Connection = conexion.AbrirConexion();
+            comand.CommandText = "ListaMembresia";
+            comand.CommandType = CommandType.StoredProcedure;
+            read = comand.ExecuteReader();
+            table.Load(read);
+            read.Close();
+            conexion.CerrarConexion();
+            return table;
+        }
+
+        //mostrar tipos de membresia
+        public DataTable ShowListTypeMembership()
+        {
+            comand.Connection = conexion.AbrirConexion();
+            comand.CommandText = "ListaTipoMembresia";
+            comand.CommandType = CommandType.StoredProcedure;
+            read = comand.ExecuteReader();
+            table.Load(read);
+            read.Close();
+            conexion.CerrarConexion();
+            return table;
+        }
+        //nueva membresia
+        public void NewMembership()
+        {
+            comand.Connection = conexion.AbrirConexion();
+            comand.CommandText = "NuevaMembresia";
+            comand.CommandType = CommandType.StoredProcedure;
+            comand.Parameters.AddWithValue("@NombreM", NameMembership);
+            comand.Parameters.AddWithValue("@Duracioon", Days);
+            comand.Parameters.AddWithValue("@IdT", IdType);
+            comand.Parameters.AddWithValue("@precio", Price);
+            comand.ExecuteNonQuery();
+            comand.Parameters.Clear();
+            conexion.CerrarConexion();
+
+        }
+        //editar membresia
+        public void EditMembership()
+        {
+            comand.Connection = conexion.AbrirConexion();
+            comand.CommandText = "editarMembresia";
+            comand.CommandType = CommandType.StoredProcedure;
+            comand.Parameters.AddWithValue("@id", ID);
+            comand.Parameters.AddWithValue("@nombre", NameMembership);
+            comand.Parameters.AddWithValue("@duracion", Days);
+            comand.Parameters.AddWithValue("@tipo", IdType);
+            comand.Parameters.AddWithValue("@precio", Price);
+            comand.ExecuteNonQuery();
+            comand.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+        //borrar membresia
+        public void DeleteMembership()
+        {
+            comand.Connection = conexion.AbrirConexion();
+            comand.CommandText = "EliminarMembresia";
+            comand.CommandType = CommandType.StoredProcedure;
+            comand.Parameters.AddWithValue("@id", ID);
+            comand.ExecuteNonQuery();
+            comand.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+    }
+}
---
+++ GYMNegocio/CNMembresia.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using GYMDatos;
+
+namespace GYMNegocio
+{
+    public class CNMembresia
+    {
+        private CDMembresia objectCD = new CDMembresia();
+
+        private String _ID;
+        private String _NameMembership;
+        private String _Days;
+        private String _IDType;
+        private String _Price;
+
+        public String ID
+        {
+            set { _ID = value; }
+            get { return _ID; }
+        }
+        public String NameMembership
+        {
+            set { _NameMembership = value; }
+            get { return _NameMembership; }
+        }
+
+        public String Days
+        {
+            set { _Days = value; }
+            get { return _Days; }
+        }
+
+        public String IdType
+        {
+            set
+            {
+                _IDType = value;
+            }
+            get
+            {   return _IDType;}
+        }
+
+        public String Price
+        {
+            set { _Price = value; }
+            get { return _Price; }
+        }
+
+        //listado de membresia
+        public DataTable ShowMembership()
+        {
+            CDMembresia objecto = new CDMembresia();
+            DataTable table = new DataTable();
+            table = objecto.ShowListMembership();
+            return table;
+        }
+        //listado de tipos
+        public DataTable ShowTypeMembership()
+        {
+            CDMembresia objectT = new CDMembresia();
+            DataTable table = new DataTable();
+            table = objectT.ShowListTypeMembership();
+            return table;
+        }
+        //nueva membresia
+        public void NewMembership()
+        {
+            objectCD.NameMembership = NameMembership;
+            objectCD.Days = Convert.ToInt32(Days);
+            objectCD.IdType = Convert.ToInt32(IdType);
+            objectCD.Price = Convert.ToDecimal(Price);
+            objectCD.NewMembership();
+        }
+        //editar membresia
+        public void EditMembership()
+        {
+            objectCD.ID = Convert.ToInt32(ID);
+            objectCD.NameMembership = NameMembership;
+            objectCD.Days = Convert.ToInt32(Days);
+            objectCD.IdType = Convert.ToInt32(IdType);
+            objectCD.Price = Convert.ToDecimal(Price);
+            objectCD.EditMembership(); 
+        }
+        //borrar membresia
+        public void DeleteMembership()
+        {
+            objectCD.ID = Convert.ToInt32(ID);
+            objectCD.DeleteMembership();
+        }
+    }
+}
